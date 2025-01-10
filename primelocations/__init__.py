f2bf941fa53675bb1435515a900ca37f0944f1df
maximylius <m.mylius@zeppelin-university.net>
2025-01-10 14:33:52 +0100

    import submodules explicitly

--- primelocations/__init__.py
+++ primelocations/__init__.py
@@ -1,19 +1,24 @@
 from importlib.metadata import version
 # TODO replace * with more expliced exports for the final version of the package 
 # from .radius_search import *
-from primelocations.radius_search.grid_class import *
-from primelocations.radius_search.optimal_grid_spacing import *
-from primelocations.radius_search.offset_regions import *
-from primelocations.utils.general import *
-from primelocations.illustrations.illustrate_nested_grid import *
-from primelocations.illustrations.illustrate_optimal_grid_spacing import *
-from primelocations.illustrations.illustrate_point_to_cell_region_assignment import *
-# from primelocations.nested_search import *
-from primelocations.radius_search.radius_search_class import *
-from primelocations.illustrations.plot_utils import *
-from primelocations.radius_search.two_dimensional_weak_ordering_class import *
+from primelocations import radius_search
+from primelocations import illustrations
+from primelocations import testing
+from primelocations import utils
+from primelocations import main
 from primelocations.main import *
-from primelocations.illustrations.plot_utils import *
+# from primelocations.radius_search.optimal_grid_spacing import *
+# from primelocations.radius_search.offset_regions import *
+# from primelocations.utils.general import *
+# from primelocations.illustrations.illustrate_nested_grid import *
+# from primelocations.illustrations.illustrate_optimal_grid_spacing import *
+# from primelocations.illustrations.illustrate_point_to_cell_region_assignment import *
+# # from primelocations.nested_search import *
+# from primelocations.radius_search.radius_search_class import *
+# from primelocations.illustrations.plot_utils import *
+# from primelocations.radius_search.two_dimensional_weak_ordering_class import *
+# from primelocations.main import *
+# from primelocations.illustrations.plot_utils import *
 # TODO ensure to no exports imports (like np.array)?
 
 # __version__ = version('primelocations')
